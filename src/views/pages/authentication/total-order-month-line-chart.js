359c8081a62d856435e6fa88bd14923f51ef9211
softdev019 <softdev019@gmail.com>
2022-06-27 09:21:04 +0900

    weg

--- src/views/pages/authentication/total-order-month-line-chart.js
+++ src/views/pages/authentication/total-order-month-line-chart.js
@@ -13,14 +13,14 @@ const chartData = {
             enabled: false
         },
         colors: ['#fff'],
-        fill: {
-            type: 'solid',
-            opacity: 1
-        },
-        stroke: {
-            curve: 'smooth',
-            width: 3
-        },
+        // fill: {
+        //     type: 'solid',
+        //     opacity: 1
+        // },
+        // stroke: {
+        //     curve: 'smooth',
+        //     width: 3
+        // },
         yaxis: {
             min: 0,
             max: 100
